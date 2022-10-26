214e744fd4c8a987d743b19c6c18419b1b634f53
joaquimnetocel <joaquim.neto.cel@gmail.com>
2022-10-26 15:23:37 -0300

    COOKIES EXAMPLE ADDED

---
+++ folderSource/folderTopics/folderExpress/folderCookies/README.md
@@ -0,0 +1,24 @@
+# COOKIES
+
+## DESCRIPTION
+
+EXAMPLE OF [_NODE.JS_](https://nodejs.org) USING:
+
+* [_COOKIES_](http://expressjs.com/en/resources/middleware/cookie-parser.html): AN HTTP COOKIE (WEB COOKIE, BROWSER COOKIE) IS A SMALL PIECE OF DATA THAT A SERVER SENDS TO A USER'S WEB BROWSER. THE BROWSER MAY STORE THE COOKIE AND SEND IT BACK TO THE SAME SERVER WITH LATER REQUESTS.
+
+## PREREQUISITES
+
+* **_NODEMON_**: FOLLOW THE INSTRUCTIONS [HERE](/folderSource/folderTopics/folderNodemon/README.md).
+* **_EXPRESS.JS_**: FOLLOW THE INSTRUCTIONS [HERE](/folderSource/folderTopics/folderExpress/README.md).
+
+## REQUIRED PACKAGES
+
+```bash
+npm install cookie-parser
+```
+
+## EXAMPLE
+
+```bash
+npx nodemon ./folderSource/folderTopics/folderExpress/folderCookies/exampleCookies.js
+```
---
+++ folderSource/folderTopics/folderExpress/folderCookies/exampleCookies.js
@@ -0,0 +1,50 @@
+// NPM MODULES
+import express from "express";
+import { createServer } from "http";
+import cookieParser from "cookie-parser";
+/////
+
+// EXPRESS
+const constExpress = express();
+/////
+
+// COOKIE PARSER
+constExpress.use(cookieParser());
+/////
+
+// ROUTES
+constExpress.get("/", function (req, res) {
+	res.setHeader("aaaa", "bbb");
+	res.send("EXPRESS SERVER RUNNING!");
+});
+constExpress.get("/routeSetCookie", function (req, res) {
+	const numberMiliseconds = 1000;
+	const numberSeconds = 60;
+	const numberMinutes = 1;
+	const numberHours = 1;
+	const numberDays = 1;
+	res.cookie(`cookieKey`, `MY COOKIE VALUE`, {
+		maxAge: numberDays * numberHours * numberMinutes * numberSeconds * numberMiliseconds,
+	});
+	res.send("THIS IS THE CONTACTS ROUTE!");
+});
+constExpress.get("/routeGetCookie", function (req, res) {
+	console.log(req.cookies.cookieKey);
+	res.send(req.cookies);
+});
+constExpress.get("/routeDeleteCookies", function (req, res) {
+	res.clearCookie();
+	res.send("Cookie has been deleted successfully");
+});
+/////
+
+// SERVER
+const constServer = createServer(constExpress);
+constServer.listen(8081, () => {
+	console.log("SERVER RUNNING WITH THE FOLLOWING ROUTES:");
+	console.log("* http://localhost:8081");
+	console.log("* http://localhost:8081/routeSetCookie");
+	console.log("* http://localhost:8081/routeGetCookie");
+	console.log("* http://localhost:8081/routeDeleteCookies");
+});
+/////
